f21b64098c00f40c7736adf1f006ea82e198a35e
Robert <rvtaylor94@gmail.com>
2023-02-20 09:34:07 -0700

    Initial commit

---
+++ src/test/java/com/galvanize/simpleautos/SimpleAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.simpleautos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
